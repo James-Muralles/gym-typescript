12ad283ed7460f5ee36043e36495a6051d8c7ada
James Muralles <james.muralles23@gmail.com>
2023-07-31 11:22:05 -0500

    added media query hook

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,9 @@
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { Bars3Icon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -13,7 +16,9 @@ const Navbar = ({
   selectedPage,
   setSelectedPage
 }: Props) => {
-    const flexBetween = "flex items-center justify-between"
+    const flexBetween = "flex items-center justify-between";
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const  isAboveMediumScreeens = useMediaQuery('(min-width: 1060px)');
   return <nav>
     <div
       className = {`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -23,6 +28,7 @@ const Navbar = ({
           {/* LEFT SIDE */}
           <img src={Logo} alt="logo" />
           {/* RIGHT SIDE */}
+          {isAboveMediumScreeens ? (
           <div className={`${flexBetween} w-full`} >
             <div className={`${flexBetween} gap-8 text-sm`} >
               <Link
@@ -45,21 +51,22 @@ const Navbar = ({
                 selectedPage={selectedPage}
                 setSelectedPage={setSelectedPage}
                 />
-
             </div>
             <div className={`${flexBetween} gap-8 text-sm`}>
               <p>Sign In</p>
               <button>Become a Member</button>
-
             </div>
-
-          </div>
-
-
+          </div> )
+          : (
+            <button
+            className="rounded-full bg-secondary-500 p-2"
+            onClick={()=> setIsMenuToggled(!isMenuToggled)}
+            >
+             <Bars3Icon className="h-6 w-6 text-white" />   
+            </button>
+          )}
          </div>
-
       </div>
-
     </nav>;
   
 };
