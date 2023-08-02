fd9f9ec3c7089de70f626656eddd65ce6919f9a5
James Muralles <james.muralles23@gmail.com>
2023-08-02 11:42:42 -0500

    finished images on home

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -15,7 +15,7 @@ type Props = {
 }
 
 const Home = ({setSelectedPage}: Props) => {
-    const isAboveMediumScreens = useMediaQuery("min-width: 1060px")
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
 
   return (
     <section
@@ -23,23 +23,23 @@ const Home = ({setSelectedPage}: Props) => {
     className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
     >
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
             {/* MAIN HEADER */}
-            <div>
+            <div className="z-10 mt-32 md:basis-3/5">
                 {/* HEADINGS */}
-                <div>
-                    <div>
-                        <div>
+                <div className="md: -mt-20">
+                    <div className="relative">
+                        <div className="before:absolute before:-top-20 before:-left-20 before:-z[-1] md:before:content-evolvetext">
                             <img src={HomePageText} alt="home-page-text" />
                         </div>
                     </div>
-                    <p>
+                    <p className="mt-8 text-sm">
                         The greatest gym ever constructed! Some say Zeus himself works out here during closing hours. Do not worry though, he makes sure to wipe down the equipment before leaving. If you want the body of a greek God you are in the right place!
                     </p>
                 </div>
 
                 {/* ACTIONS */}
-                <div>
+                <div className="mt-8 flex items-center gap-8">
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -54,13 +54,27 @@ const Home = ({setSelectedPage}: Props) => {
                 </div>
             </div>
             {/* IMAGE */}
-            <div>
-
-            </div>
+                <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+                    <img alt="home-pageGraphic" src={HomePageGraphic}/>
+                </div>
         </div>
+        {/* SPONSORS */}
+        
+        {isAboveMediumScreens &&(
+            <div className="h-[150px] w-full bg-primary-100 py-10">
+                <div className="mx-auto w-5/6">
+                    <div className="flex w-3/5 items-center justify-between gap-8">
+                        <img src={SponsorRedBull} alt="redbull-sponsor " />
+                        <img src={SponsorForbes} alt="forbes-sponsor " />
+                        <img src={SponsorFortune} alt="fortune-sponsor " />
+                    </div>
+                </div>
+            </div>
+
+        )}
 
     </section>
-  )
-}
+  );
+};
 
 export default Home
