ea5f4579c6dcda7459ad90bb8516717d94408941
James Muralles <james.muralles23@gmail.com>
2023-08-03 11:23:12 -0500

    created benefits cards

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,13 +1,31 @@
 
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import { 
     HomeModernIcon, 
     UserGroupIcon, 
     AcademicCapIcon,
 } from "@heroicons/react/24/solid";
-import { secureHeapUsed } from "crypto";
 import { motion } from "framer-motion";
+import Benefit from "./benefit";
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6"/>,
+        title: "State of the Art Facilities",
+        description: "Built by Tony Stark himself."
+     },
+     {
+        icon: <UserGroupIcon className="h-6 w-6"/>,
+        title: "100s of diverse classes",
+        description: "Learn to fly, climb walls, or harness that electricity you cannot quite control yet"
+     },
+     {
+        icon: <AcademicCapIcon className="h-6 w-6"/>,
+        title: "Expert and Pro trainers",
+        description: "Learn from Goku and Vegeta. Sometimes even All Might!"
+     }
+];
 
 
 type Props = {
@@ -24,14 +42,28 @@ const Benefits = ({setSelectedPage}: Props) => {
     className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
     onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
 
-        <div>
+
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
             <HText>MORE THAN A GYM.</HText>
-            <p>
+            <p className="my- 5 text-sm">
                 We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.
             </p>
         </div>
 
-    </motion.div>
+        {/* Benefits */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+            {benefits.map((benefit: BenefitType) =>(
+                <Benefit
+                key={benefit.title}
+                icon={benefit.icon}
+                title={benefit.title}
+                description={benefit.description}
+                setSelectedPage={setSelectedPage}
+                />
+            ))}
+        </div>
+    </motion.div>   
    </section>
   )
 }
