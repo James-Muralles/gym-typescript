debb3f243f7ee96022470f000445be0fa80fe5a0
James Muralles <james.muralles23@gmail.com>
2023-08-07 11:34:55 -0500

    finished OurClasses page

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,4 +1,4 @@
-import { SelectedPage } from "@/shared/types"
+import { ClassType, SelectedPage } from "@/shared/types"
 import image1 from '@/assets/image1.png'
 import image2 from '@/assets/image2.png'
 import image3 from '@/assets/image3.png'
@@ -7,6 +7,39 @@ import image5 from '@/assets/image5.png'
 import image6 from '@/assets/image6.png'
 import { motion } from "framer-motion"
 import HText from "@/shared/HText"
+import Class from "./Class"
+
+const classes:Array<ClassType> = [
+    {
+        name: "Weight Training Classes",
+        description: "A class taught by the Rock.",
+        image: image1
+    },
+    {
+        name: "Yoga Classes",
+        description: "A class taught by the Rock.",
+        image: image2
+    },
+    {
+        name: "Ab Core Classes",
+        image: image3
+    },
+    {
+        name: "Adventure Classes",
+        description: "A class taught by the Rock.",
+        image: image4
+    },
+    {
+        name: "Fitness Classes",
+        description: "A class taught by the Rock.",
+        image: image5
+    },
+    {
+        name: "Training Classes",
+        description: "A class taught by the Rock.",
+        image: image6
+    }
+]
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
@@ -35,8 +68,16 @@ const OurClasses = ({setSelectedPage}: Props) => {
                 </div>
 
             </motion.div>
-            <div className="mt-1 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+            <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
                 <ul className="w-[2800px] whitespace-nowrap">
+                    {classes.map((item: ClassType, index) =>(
+                        <Class
+                        key={`${item.name}-${index}`}
+                        name={item.name}
+                        description={item.description}
+                        image={item.image}
+                        />
+                    ))}
 
                 </ul>
             </div>
